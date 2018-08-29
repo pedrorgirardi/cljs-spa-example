1244130a26ee939aef683f174c92f3ca12f66e60
Pedro Girardi <pedro.rogerio.girardi@gmail.com>
2018-08-29 22:02:20 +0700

    Make Clock page work with re-frame

--- src/cljs_spa/page/clock.cljs
+++ src/cljs_spa/page/clock.cljs
@@ -1,25 +1,41 @@
 (ns cljs-spa.page.clock
   (:require [cljs-spa.util :as util]
-            [cljs-spa.state :refer [!state]]))
+            [cljs-spa.state :refer [!state]]
+            [re-frame.core :as rf]))
 
 (defn tick []
   (js/console.log "Tick")
-  (swap! !state assoc :clock-time (int (/ (.valueOf (js/Date.)) 1000))))
+
+  (rf/dispatch [::!clock-time (int (/ (.valueOf (js/Date.)) 1000))]))
 
 (defn activate []
   (tick)
-  (swap! !state update :clock
-         (fn [interval] (util/set-interval interval tick 1000))))
+
+  (rf/dispatch [::!clock (fn [interval]
+                           (util/set-interval interval tick 1000))]))
 
 (defn deactivate []
-  (swap! !state update :clock util/clear-interval))
+  (rf/dispatch [::!clock util/clear-interval]))
 
 (defn page-ui []
   [:div
    [:h3 "Clock"]
-   [:div "Seconds since epoch: " (-> @!state :clock-time)]
+   [:div "Seconds since epoch: " @(rf/subscribe [::?clock-time])]
    [:div
     "This pages demonstrates acquiring and disposing of resources. When the
 user enters the page, a setInterval timer is created. While active, you can see
 Tick messages in the console log. When the user navigates away (e.g. to #/home)
 the timer is cleared."]])
+
+
+(rf/reg-event-db ::!clock-time
+  (fn [db [_ clock-time]]
+    (assoc-in db [:page/clock :clock-time] clock-time)))
+
+(rf/reg-sub ::?clock-time
+  (fn [db _]
+    (get-in db [:page/clock :clock-time])))
+
+(rf/reg-event-db ::!clock
+  (fn [db [_ f]]
+    (update-in db [:page/home :clock] f)))
